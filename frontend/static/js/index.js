941eefdb9a7900472cc78c6a879fc07d19f9f220
Dima <dchaika1990@gmail.com>
2021-04-01 23:24:52 +0300

    start router module

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,29 @@
-console.log('js is load')
+const router = async () => {
+	const routes = [
+		{path: "/", view: () => console.log('Viewing dashboard')},
+		{path: "/posts", view: () => console.log('Viewing posts')},
+		{path: "/settings", view: () => console.log('Viewing settings')}
+	]
+
+	// Test each route for potential match
+	const potentialMatches = routes.map( route => {
+		return {
+			route,
+			isMatch: location.pathname === route.path
+		}
+	} )
+
+	let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+	if (!match) {
+		match = {
+			route: routes[0],
+			isMatch: true
+		}
+	}
+
+	console.log(match)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	router();
+})
